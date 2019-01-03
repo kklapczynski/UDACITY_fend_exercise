e9a51e3f4471db3a7a81558c6929f071b533d772
Krzysztof Klapczyński <k.klapczynski@gmail.com>
2019-01-03 12:54:00 +0100

    refactor: Add list of cats and display cliked cat
    Adds new cats to project. Adds list of their names.
    Photo of each is diplayed when name cliked on the list.
    Clicks on each photo are counted and displayed.
    
    Resolves: Requirements 3

--- js/app.js
+++ js/app.js
@@ -13,23 +13,50 @@ cats = [
         imgPath: 'images\\cat_2_large.jpg',
         numberOfClicks: 0,
         addClick() {this.numberOfClicks++;}
+    },
+    {
+        id: 2,
+        name: 'JJ',
+        imgPath: 'images\\cat_3_large.jpg',
+        numberOfClicks: 0,
+        addClick() {this.numberOfClicks++;}
+    },
+    {
+        id: 3,
+        name: 'DD',
+        imgPath: 'images\\cat_4_large.jpg',
+        numberOfClicks: 0,
+        addClick() {this.numberOfClicks++;}
+    },
+    {
+        id: 4,
+        name: 'Iffy',
+        imgPath: 'images\\cat_5_large.jpg',
+        numberOfClicks: 0,
+        addClick() {this.numberOfClicks++;}
     }
 ];
-
-// setting photos to display
-const container = document.getElementsByClassName('container').item(0);
+// setting cats list
+const catsList = document.getElementsByClassName("cats-list").item(0);
 for (cat of cats) {
-    const photoHtml = `
-        <div class="photo">
-            <img class="cat-photo" id= "${cat.id}" src="${cat.imgPath}" alt="Little cat photo">
-            <p id="cat-info-${cat.id}" class="clicks-info">${cat.name} was clicked ${cat.numberOfClicks} times.</p>
-        </div>
-        <div class="divider"></div>
+    const catNameHtml = `
+        <li id="cat-name-${cat.id}">${cat.name}</li>
     `;
-    container.insertAdjacentHTML('beforeend', photoHtml);
+    catsList.insertAdjacentHTML('beforeend', catNameHtml);
 }
 
+
+
+
 // event listeners
+
+// show photo of cat cliked on the list
+document.getElementsByClassName('cats-list').item(0).addEventListener('click', (e) => {
+    const target = e.target;
+    const catId = parseInt(target.id.replace('cat-name-', ''));
+    displayCat(catId);
+})
+
 document.getElementsByClassName('container').item(0).addEventListener('click', (e) => {
     const targetCat = e.target;
     const catId = targetCat.id;
@@ -46,6 +73,40 @@ updateClicksNumber = (id) => {
 };
 
 // update view
+const catPhotoContainer = document.getElementsByClassName('container').item(0);
+displayCat = (id) => {
+    // if there is already cat displayed and it's other then clicked one remove it
+    if(isCatDisplayed()) {
+        if(!isClickedCat(id)) {
+            removeDisplayedCat();
+        } else {
+            return;
+        }
+    }
+
+    const cat = cats.filter(item => {return item.id == id; })[0];
+    const photoHtml = `
+        <div class="photo">
+            <img class="cat-photo" id= "${cat.id}" src="${cat.imgPath}" alt="Little cat photo">
+            <p id="cat-info-${cat.id}" class="clicks-info">${cat.name} was clicked ${cat.numberOfClicks} times.</p>
+        </div>
+    `;
+    catPhotoContainer.insertAdjacentHTML('beforeend', photoHtml);
+}
+
+isCatDisplayed = () => {
+    return document.getElementsByClassName('photo').length > 0 ? true : false;
+}
+
+isClickedCat = (id) => {
+    return document.getElementById(id) ? true : false;
+}
+
+removeDisplayedCat = () => {
+    document.getElementsByClassName('photo').item(0).remove();
+    return;
+}
+
 updateClickNumberView = (id, cat) => {
     document.getElementById(`cat-info-${id}`).textContent = `${cat.name} was clicked ${cat.numberOfClicks} times.`;
     return;
