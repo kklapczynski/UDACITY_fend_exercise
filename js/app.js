71e2c20aa78cf2cf30a9dcc4de0977f88cac0cd9
Krzysztof Klapczyński <k.klapczynski@gmail.com>
2019-01-03 22:40:22 +0100

    refactor: Rebuild to MVC architecture
    Adds model, controller, catListview and catView to realize separation
    of concerns rule.
    
    Resolves: Cat Clicker Premium Specs

--- js/app.js
+++ js/app.js
@@ -1,114 +1,129 @@
+(function() {
+    const model = {
+        cats: [
+            {
+                id: 0,
+                name: 'Kitty',
+                imgPath: 'images\\cat_large.jpg',
+                numberOfClicks: 0,
+                addClick() {this.numberOfClicks++;}
+            },
+            {
+                id: 1,
+                name: 'Chewie',
+                imgPath: 'images\\cat_2_large.jpg',
+                numberOfClicks: 0,
+                addClick() {this.numberOfClicks++;}
+            },
+            {
+                id: 2,
+                name: 'JJ',
+                imgPath: 'images\\cat_3_large.jpg',
+                numberOfClicks: 0,
+                addClick() {this.numberOfClicks++;}
+            },
+            {
+                id: 3,
+                name: 'DD',
+                imgPath: 'images\\cat_4_large.jpg',
+                numberOfClicks: 0,
+                addClick() {this.numberOfClicks++;}
+            },
+            {
+                id: 4,
+                name: 'Iffy',
+                imgPath: 'images\\cat_5_large.jpg',
+                numberOfClicks: 0,
+                addClick() {this.numberOfClicks++;}
+            }
+        ],
+        updateClicksNumber(id) {
+            const cat = this.cats.filter(item => { return item.id == id; })[0];
+            cat.addClick();
+            return cat;
+        }
+    };
 
-cats = [
-    {
-        id: 0,
-        name: 'Kitty',
-        imgPath: 'images\\cat_large.jpg',
-        numberOfClicks: 0,
-        addClick() {this.numberOfClicks++;}
-    },
-    {
-        id: 1,
-        name: 'Chewie',
-        imgPath: 'images\\cat_2_large.jpg',
-        numberOfClicks: 0,
-        addClick() {this.numberOfClicks++;}
-    },
-    {
-        id: 2,
-        name: 'JJ',
-        imgPath: 'images\\cat_3_large.jpg',
-        numberOfClicks: 0,
-        addClick() {this.numberOfClicks++;}
-    },
-    {
-        id: 3,
-        name: 'DD',
-        imgPath: 'images\\cat_4_large.jpg',
-        numberOfClicks: 0,
-        addClick() {this.numberOfClicks++;}
-    },
-    {
-        id: 4,
-        name: 'Iffy',
-        imgPath: 'images\\cat_5_large.jpg',
-        numberOfClicks: 0,
-        addClick() {this.numberOfClicks++;}
-    }
-];
-// setting cats list
-const catsList = document.getElementsByClassName("cats-list").item(0);
-for (cat of cats) {
-    const catNameHtml = `
-        <li id="cat-name-${cat.id}">${cat.name}</li>
-    `;
-    catsList.insertAdjacentHTML('beforeend', catNameHtml);
-}
+    const controller = {
+        init() {
+            catListView.init(model.cats);
+            this.setEventListeners();
+        },
+        // event listeners
+        setEventListeners() {
+            // show photo of cat cliked on the list
+            document.getElementsByClassName('cats-list').item(0).addEventListener('click', (e) => {
+                const target = e.target;
+                const catId = parseInt(target.id.replace('cat-name-', ''));
+                const cat = model.cats.filter(item => {return item.id == catId; })[0];
+                catView.displayCat(cat);
+            });
 
+            document.getElementsByClassName('photo').item(0).addEventListener('click', (e) => {
+                const targetCat = e.target;
+                const catId = targetCat.id;
+                const cat = model.updateClicksNumber(catId);
+                catView.updateClickNumberView(catId, cat);
+                return;
+            });
+        }
 
+    };
 
+    const catListView = {
+        init(data) {
+            this.displayCatsList(data);
+        },
+        // setting cats list
+        displayCatsList(cats) {
+            const catsList = document.getElementsByClassName("cats-list").item(0);
+            for (cat of cats) {
+                const catNameHtml = `
+                    <li id="cat-name-${cat.id}">${cat.name}</li>
+                `;
+                catsList.insertAdjacentHTML('beforeend', catNameHtml);
+            }
+        }
 
-// event listeners
+    };
 
-// show photo of cat cliked on the list
-document.getElementsByClassName('cats-list').item(0).addEventListener('click', (e) => {
-    const target = e.target;
-    const catId = parseInt(target.id.replace('cat-name-', ''));
-    displayCat(catId);
-})
+    const catView = {
+        catPhotoDiv: document.getElementsByClassName('photo').item(0),
+        displayCat(cat) {
+            // if there is already cat displayed and it's other then clicked one remove it
+            if(this.isCatDisplayed()) {
+                if(!this.isClickedCat(cat.id)) {
+                    this.removeDisplayedCat();
+                } else {
+                    return;
+                }
+            }
 
-document.getElementsByClassName('container').item(0).addEventListener('click', (e) => {
-    const targetCat = e.target;
-    const catId = targetCat.id;
-    const cat = updateClicksNumber(catId);
-    updateClickNumberView(catId, cat);
-    return;
-});
+            const photoHtml = `
+                    <img class="cat-photo" id= "${cat.id}" src="${cat.imgPath}" alt="Little cat photo">
+                    <p id="cat-info-${cat.id}" class="clicks-info">${cat.name} was clicked ${cat.numberOfClicks} times.</p>
+            `;
+            this.catPhotoDiv.insertAdjacentHTML('beforeend', photoHtml);
+        },
 
-// update data
-updateClicksNumber = (id) => {
-    const cat = cats.filter(item => { return item.id == id; })[0];
-    cat.addClick();
-    return cat;
-};
+        isCatDisplayed() {
+            return document.getElementsByClassName('cat-photo').length > 0 ? true : false;
+        },
 
-// update view
-const catPhotoContainer = document.getElementsByClassName('container').item(0);
-displayCat = (id) => {
-    // if there is already cat displayed and it's other then clicked one remove it
-    if(isCatDisplayed()) {
-        if(!isClickedCat(id)) {
-            removeDisplayedCat();
-        } else {
+        isClickedCat(id) {
+            return document.getElementById(id) ? true : false;
+        },
+
+        removeDisplayedCat() {
+            document.getElementsByClassName('cat-photo').item(0).remove();
+            document.getElementsByClassName('clicks-info').item(0).remove();
+            return;
+        },
+
+        updateClickNumberView(id, cat) {
+            document.getElementById(`cat-info-${id}`).textContent = `${cat.name} was clicked ${cat.numberOfClicks} times.`;
             return;
         }
-    }
-
-    const cat = cats.filter(item => {return item.id == id; })[0];
-    const photoHtml = `
-        <div class="photo">
-            <img class="cat-photo" id= "${cat.id}" src="${cat.imgPath}" alt="Little cat photo">
-            <p id="cat-info-${cat.id}" class="clicks-info">${cat.name} was clicked ${cat.numberOfClicks} times.</p>
-        </div>
-    `;
-    catPhotoContainer.insertAdjacentHTML('beforeend', photoHtml);
-}
-
-isCatDisplayed = () => {
-    return document.getElementsByClassName('photo').length > 0 ? true : false;
-}
-
-isClickedCat = (id) => {
-    return document.getElementById(id) ? true : false;
-}
-
-removeDisplayedCat = () => {
-    document.getElementsByClassName('photo').item(0).remove();
-    return;
-}
-
-updateClickNumberView = (id, cat) => {
-    document.getElementById(`cat-info-${id}`).textContent = `${cat.name} was clicked ${cat.numberOfClicks} times.`;
-    return;
-}
-
+    };
+    controller.init();
+})();
